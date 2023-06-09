a0cb59af4cd7c3e8b02cfb88b0590306f1bdcb09
Evandro Fernandes <evandrofidk@hotmail.com>
2023-06-09 03:00:07 +0000

    refactor(csv)::hammer: remove wrongs csv

--- input/desafio/pre_process/download.sh
+++ input/desafio/pre_process/download.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-array=("alunos" "categoria" "cidade" "cliente" "estado" "filial" "item_pedido" "parceiro" "produto" "subcategoria")
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONFIG="${BASEDIR}/../config/config.sh"
+source "${CONFIG}"
 
-teste=("cidade" "categoria")
-for ((i=0;i <${#teste[@]};i++))
+echo "Iniciando a criacao em ${DATE}"
+echo "Com directorio ${BASEDIR}"
+
+for table in "${TABLES[@]}"
 do
-cd ../raw
-item=${teste[i]}
-mkdir $item
-chmod 777 $item
-cd $item
-curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/$item.csv
-cd ..
+    echo "navegando na estrutua de pastas."
+    cd ../../raw/
+
+    echo "criando a pasta $table"
+    mkdir $table
+
+    echo "dando permissão"
+    chmod 777 $$table
+
+    cd $table
+
+    echo "baixando tabela $table ..."
+    curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/$table.csv
+
 done
 
 
+echo "Finalizando a criacao em ${DATE}"
